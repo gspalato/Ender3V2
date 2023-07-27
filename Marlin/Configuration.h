7626417b448fbb70ec11bdee7152634291b773fc
Gabriel Spalato <3950465+gspalato@users.noreply.github.com>
2023-07-27 15:56:17 -0300

    Changed printer name.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -69,7 +69,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "creality" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Creality & gspalato" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -143,7 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 V2" 
+#define CUSTOM_MACHINE_NAME "Stasis" 
 
 
 
